6e7dae6da5d01b8658eb054486b903cac861702f
csaguil1 <45695668+csaguil1@users.noreply.github.com>
2019-06-16 03:28:16 -0400

    holy heck img of enemies added and man game works

--- game.py
+++ game.py
@@ -39,6 +39,7 @@ bg4 = pygame.image.load("scene 3.png")
 bg5 = pygame.image.load("scene 4.png")
 winbg = pygame.image.load("win.png")
 losebg = pygame.image.load("lose.png")
+raf1 = pygame.image.load("raf 1.png")
 
 def text_objects(text, font, color):
     """displays text"""
@@ -109,6 +110,80 @@ def scene7():
     text_box("you only took one step forward?? ok i guess ://")
     pygame.display.update()
 
+def scene8():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    text_box("right next to us is the office, and then inside the office is the principal's office")
+    pygame.display.update()
+
+def scene9():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    text_box("offiception??")
+    pygame.display.update()
+
+def scene10():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    text_box("woah wait a second, i think i spot a wild raf in his natural habitat")
+    pygame.display.update()
+
+def scene11():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("raf: H E L L O. I see you're a new student here.")
+    pygame.display.update()
+
+def scene12():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("raf: i'm kind of the principal around here and as you should know we're an engineering school.")
+    pygame.display.update()
+
+def scene13():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("raf: and you know what they say where there's engineering there's calc.")
+    pygame.display.update()
+
+def scene14():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("you've never heard that saying before but might as well pretend")
+    pygame.display.update()
+
+def scene15():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("raf: therefore, let me ask you some calc questions to see whether or not you really belong at this school.")
+    pygame.display.update()
+
+def scene16():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("raf: ok so when you do a derivative, you are finding the ___ of a curve?")
+    pygame.display.update()
+
+def scene17():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("raf: press 1 [slope], 2 [area], 3 [volume].")
+    pygame.display.update()
+
+def scene18():
+    """opening scene"""
+    window.blit(bg2, (0,0))
+    window.blit(raf1, (100, 100))
+    text_box("great job, lets move on to the next question")
+    pygame.display.update()
+
 run = False
 
 while not run:
@@ -137,6 +212,36 @@ while not run:
         scene6()
     elif count == 6:
         scene7()
+    elif count == 7:
+        scene8()
+    elif count == 8:
+        scene9()
+    elif count == 9:
+        scene10()
+    elif count == 10:
+        scene11()
+    elif count == 11:
+        scene12()
+    elif count == 12:
+        scene13()
+    elif count == 13:
+        scene14()
+    elif count == 14:
+        scene15()
+    elif count == 15:
+        scene16()
+    elif count == 16:
+        scene17()
+        if key[pygame.K_1]:
+            count += 1
+        if key[pygame.K_2]:
+            count == 500
+        if key[pygame.K_3]:
+            count == 500
+    elif count == 17:
+        scene18()
+    elif count == 500:
+        lose()
 
 pygame.quit()
 quit()
